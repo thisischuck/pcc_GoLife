cd8d60316739e6a76d099e7d8952173f34de17ff
Chuck <c.costa.gdev@gmail.com>
2021-06-17 20:19:35 +0100

    fooled around to add more planes
    cause max rendertexture size is about 16k

--- GameOfLife/Assets/Scripts/GameOfLife.cs
+++ GameOfLife/Assets/Scripts/GameOfLife.cs
@@ -7,7 +7,7 @@ using System;
 
 public class GameOfLife : MonoBehaviour
 {
-    public Vector2 gridSize;
+    public Vector2 gridSize = new Vector2(15360, 8640);
     public bool isDrawing;
     [Range(0, 100)]
     public int mutationPercentage;
@@ -20,24 +20,62 @@ public class GameOfLife : MonoBehaviour
 
     #region ComputeShader
 
-    public ComputeShader lifeShader;
-    public ComputeShader startShader;
-    public ComputeShader copyShader;
-    public RenderTexture result;
-    public RenderTexture start;
+    private ComputeShader[] lifeShaders;
+    private ComputeShader[] startShaders;
+    private ComputeShader[] copyShaders;
+    public RenderTexture[] results;
+    public RenderTexture[] starts;
 
-    public GameObject plane;
+    public List<GameObject> planes;
 
     int k;
     #endregion
 
+    public ComputeShader lifeShader;
+    public ComputeShader startShader;
+    public ComputeShader copyShader;
+
+    void Fill(ComputeShader[] array, int tag)
+    {
+        for (int i = 0; i < planes.Count; i++)
+        {
+            switch (tag)
+            {
+                case 0:
+                    array[i] = (ComputeShader)Instantiate(startShader);
+                    continue;
+                case 1:
+                    array[i] = (ComputeShader)Instantiate(lifeShader);
+                    continue;
+                case 2:
+                    array[i] = (ComputeShader)Instantiate(copyShader);
+                    continue;
+            }
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        results = new RenderTexture[planes.Count];
+        starts = new RenderTexture[planes.Count];
+        lifeShaders = new ComputeShader[planes.Count];
+        startShaders = new ComputeShader[planes.Count];
+        copyShaders = new ComputeShader[planes.Count];
+
+        Fill(startShaders, 0);
+        Fill(lifeShaders, 1);
+        Fill(copyShaders, 2);
+
         k = lifeShader.FindKernel("CSMain");
         gridWitdh = (int)gridSize.x;
         gridHeight = (int)gridSize.y;
-        CreateGame();
+
+
+        for (int i = 0; i < planes.Count; i++)
+        {
+            CreateGame(planes[i], i);
+        }
     }
 
     RenderTexture SetupRenderTexture()
@@ -51,46 +89,52 @@ public class GameOfLife : MonoBehaviour
         return t;
     }
 
-    void CreateGame()
+    void CreateGame(GameObject p, int id)
     {
-        lifeShader.SetFloats("size", gridWitdh, gridWitdh);
-        start = SetupRenderTexture();
 
-        startShader.SetTexture(k, "Result", start);
-        startShader.Dispatch(k, gridWitdh / 8, gridHeight / 8, 1);
-        plane.GetComponent<MeshRenderer>().material.mainTexture = start;
+        lifeShaders[id].SetFloats("size", gridWitdh, gridWitdh);
+        starts[id] = SetupRenderTexture();
 
-        result = SetupRenderTexture();
-        lifeShader.SetTexture(k, "Start", start);
-        lifeShader.SetTexture(k, "Result", result);
+        startShaders[id].SetTexture(k, "Result", starts[id]);
+        startShaders[id].Dispatch(k, gridWitdh / 8, gridHeight / 8, 1);
+        p.GetComponent<MeshRenderer>().material.mainTexture = starts[id];
+
+        results[id] = SetupRenderTexture();
+        lifeShaders[id].SetTexture(k, "Start", starts[id]);
+        lifeShaders[id].SetTexture(k, "Result", results[id]);
     }
 
-    void DrawTexture()
+    void DrawTexture(GameObject p, int id)
     {
-        lifeShader.SetTexture(k, "Start", start);
-        lifeShader.Dispatch(k, gridWitdh / 8, gridHeight / 8, 1);
+        lifeShaders[id].SetTexture(k, "Start", starts[id]);
+        lifeShaders[id].Dispatch(k, gridWitdh / 8, gridHeight / 8, 1);
 
-        plane.GetComponent<MeshRenderer>().material.mainTexture = result;
+        p.GetComponent<MeshRenderer>().material.mainTexture = results[id];
 
-        copyShader.SetTexture(
+        copyShaders[id].SetTexture(
             k,
             "Dest",
-            start
+            starts[id]
         );
 
-        copyShader.SetTexture(
+        copyShaders[id].SetTexture(
             k,
             "Source",
-            result
+            results[id]
         );
 
-        copyShader.Dispatch(k, gridWitdh / 8, gridHeight / 8, 1);
+        copyShaders[id].Dispatch(k, gridWitdh / 8, gridHeight / 8, 1);
     }
 
     void Update()
     {
         if (isDrawing)
-            DrawTexture();
+        {
+            for (int i = 0; i < planes.Count; i++)
+            {
+                DrawTexture(planes[i], i);
+            }
+        }
 
         frameCount++;
         timePassed += Time.deltaTime;
